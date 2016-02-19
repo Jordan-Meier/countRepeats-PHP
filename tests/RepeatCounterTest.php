40aba73cc9fce2262e4ae068f18686966d9fce86
Jordan Meier <jbirdmeier@gmail.com>
2016-02-19 08:08:15 -0800

    add initial project files and folders

---
+++ tests/RepeatCounterTest.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once "src/RepeatCounter.php";
+
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_method_breakitdown()
+        {
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $string = ;
+            $find_word = ;
+
+            //Act
+            $result = $test_RepeatCounter->playGame($string, $find_word);
+
+            //Assert
+            $this->assertEquals(, $result1);
+
+    }
+
+?>
